91080d1ae432612b7adfd0a7d0962e536ad4a38f
Alberto Valencia <alvalenc@ucsd.edu>
2022-10-26 23:26:58 -0700

    trying to display horn image

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -4,4 +4,20 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
-}
+  /*
+  const img = new Image();   // Create new img element
+  img.addEventListener('load', () => {
+    drawimage()
+  }, false);
+  img.src = 'party-horn.svg'
+  */
+  /*
+  const selectElement = document.querySelector('horn');
+  selectElement.addEventListener('change', (event) => {
+  const result = document.querySelector('.src');
+  result.
+  */
+   
+  
+  }
+
