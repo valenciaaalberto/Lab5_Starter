a5cb769ea8a0eb03975af2344a51ff6499a846c4
Alberto Valencia <alvalenc@ucsd.edu>
2022-10-27 11:12:25 -0700

    updated visual and audio

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -7,18 +7,16 @@ function init() {
       selectButton.addEventListener('click', () =>{
       audio.play();
   });
+
   const selectElement = document.getElementById('horn-select');
   selectElement.addEventListener('change', (event) => {
     if(event.target.value == "air-horn"){
       displayImg("assets/images/air-horn.svg");
-      
     }else if(event.target.value == "car-horn"){
       displayImg("assets/images/car-horn.svg")
-      const selectButton = document.getElementById('soundButton');
       audio = new Audio("assets/audio/car-horn.mp3");
     }else if(event.target.value == "party-horn"){
       displayImg("assets/images/party-horn.svg")
-      const selectButton = document.getElementById('soundButton');
       audio = new Audio("assets/audio/party-horn.mp3");
     }
   });
